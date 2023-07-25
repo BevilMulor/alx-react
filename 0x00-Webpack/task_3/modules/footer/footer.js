067ee40cdc1f36eeb39b5708a7ea506288abdd03
BevilMulor <109423584+BevilMulor@users.noreply.github.com>
2023-07-25 18:59:36 +0200

    Create footer.js

---
+++ 0x00-Webpack/task_3/modules/footer/footer.js
@@ -0,0 +1,7 @@
+// interesting jquery import thanks to babel
+import $ from "jquery";
+// styles
+import "./footer.css";
+
+// append elements
+$("body").append(`<p>Copyright - Holberton School</p>`);
